4773fe16ddf01e5a92e373e518f286ef12dde614
Preston Higginbotham <prestonhigg17@gmail.com>
2021-02-05 14:13:33 -0600

    RandomNumberDistribution

--- src/com/gmail/prestonhigg17/InsertSort.java
+++ src/com/gmail/prestonhigg17/InsertSort.java
@@ -5,7 +5,17 @@ import java.util.ArrayList;
 public class InsertSort
 {
     private ArrayList<Integer> sortArray = new ArrayList<Integer>();
-    
+    RandNum number = new RandNum();
+
+    public void createArray(int size)
+    {
+        for (int index = 0; index < size; index++) ;
+        {
+            sortArray.add(index, number.getNextBetween(1, 10));
+        }
+        System.out.println(sortArray);
+    }
+
     public void insertSort()
     {
 
