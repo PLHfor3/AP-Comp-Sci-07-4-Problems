15dfca11126d3fc91805b28f22ad251c8a80f941
Preston Higginbotham <prestonhigg17@gmail.com>
2021-02-08 20:32:18 -0600

    Final Commit 1.1

--- src/com/gmail/prestonhigg17/DeleteDupes.java
+++ src/com/gmail/prestonhigg17/DeleteDupes.java
@@ -6,16 +6,27 @@ public class DeleteDupes
 {
     public ArrayList<Integer> removeDups(ArrayList<Integer> arrayRemove)
     {
-        for (int index = arrayRemove.size() - 1; index > 0; index--)
+        int index = arrayRemove.size() - 1;
+        while (index > 0)
         {
+            int numberDeleted = 0;
             int examine = arrayRemove.get(index);
-            for (int j = arrayRemove.size() - 2; j > -1; j--)
+            for (int j = index - 1; j > -1; j--)
             {
                 if (examine == arrayRemove.get(j) && index != j)
                 {
                     arrayRemove.remove(j);
+                    numberDeleted++;
                 }
             }
+            if (numberDeleted > 0)
+            {
+                index -= numberDeleted;
+            } else
+            {
+                index -= 1;
+            }
+
         }
         return arrayRemove;
     }
